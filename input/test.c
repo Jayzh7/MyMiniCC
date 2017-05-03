5496fa5c13b03dbd15db68b67cdd937a53c399b7
Jayzh7 <jayzh7@hotmail.com>
2017-05-03 08:49:15 +0800

    unknown update

--- input/test.c
+++ input/test.c
@@ -1,5 +1,8 @@
 int main(int a, int b){	//main function
 	int a =10e10;
+	int b = -1.234;
+	int c = -123.345;
+	int e = 1e23;
 	int str = "123";
     return a + b;
 }
--- input/test.pp.c
+++ input/test.pp.c
@@ -1,5 +1,8 @@
 int main ( int a , int b ) { 
  int a = 10e10 ; 
+ int b = - 1.234 ; 
+ int c = - 123.345 ; 
+ int e = 1e23 ; 
  int str = "123" ; 
  return a + b ; 
  } 
